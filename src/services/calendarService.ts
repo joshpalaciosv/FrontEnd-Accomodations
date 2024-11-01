1e877f287ab34036287e68c390a8bc6e0c85ac30
jshpalacios <jshpalacios@gmail.com>
2024-11-01 16:45:28 -0600

    detallitos sobre calenario

--- src/services/calendarService.ts
+++ src/services/calendarService.ts
@@ -1,71 +1,81 @@
 import moment from "moment";
 import { CalendarBooking, CalendarEntry } from "../interfaces/calendar.interface";
 import { apiBookings } from "./apiBookings";
-//import AccommodationsPage from "../pages/dashboard/AccommodationsPage";
 
 export const getCalendarEntries = async (accomodationId?: string) => {
    try {
-     //const accomodations = await getAllAccommodations();
+     // obtengo el token de la sesion
      const token = sessionStorage.getItem('token') ? JSON.parse(sessionStorage.getItem('token')!) : null;
    
+     // si no hay token, devuelvo un array vacio
      if (!token) {
           console.error(token);
           return [];
      }
 
-     //console.log(accomodations);
+     // inicializo el array de eventos
      const events: CalendarEntry[] = [];
-     //const events = [];
 
-     //for (const accomodation of accomodations) {
+     
 
           try {
-               //const { data } = (await apiBookings.get<CalendarBooking[]>(`/api/V1/bookings/calendar/${accomodation.id}`)) ?? [];
-               //return data;
-               const { data } = (await apiBookings.get<CalendarBooking[]>(
+               
+              // obtenemos las reservas del alojamiento indicado en el accomodationId
+
+               const response = await apiBookings.get<CalendarBooking[]>(
                     `/api/V1/bookings/calendar/${accomodationId}`,
                     {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
+                      headers: {
+                        Authorization: `Bearer ${token}`
+                      }
                     }
-                )) ?? [];
+                  );
+               
+               // si la respuesta es correcta, mapeamos los datos y los agregamos al array de eventos
+               const { data, status } = response;
 
-               //console.log(accomodation.id);
-               //console.log(data);
+               if (status === 200) { 
+                    // obtenemos la informacion que necesitamos para mostrar en el calendario
+                    const mappedData = data.map(booking => ({
+                         //id: booking.id,
+                         status: booking.status,
+                         title: booking.booking,
+                         start: moment(booking.check_in_date).toDate(),
+                         end: moment(booking.check_out_date).toDate(),
+                    }));
 
-               // Map the data to the desired structure and push to events array
-               const mappedData = data.map(booking => ({
-                    //id: booking.id,
-                    status: booking.status,
-                    title: booking.booking,
-                    start: moment(booking.check_in_date).toDate(),
-                    end: moment(booking.check_out_date).toDate(),
-               }));
-
-               //console.log(mappedData);
-
-               events.push(...mappedData);
-               console.log(events);
+                    // agregamos los eventos al array de events.
+                    events.push(...mappedData);
+                    //console.log(events);
+               }
+               else
+               {
+                    console.error(`Error: Recibimos el status de Error # ${status}`);
+                    return [];
+               }
+                              
 
           }
-          catch (error)
+          catch (error: any)
           {
-               if (error.response.status = 401) {
+               // si el error es 401, devolvemos un array vacio
+               // tratamos de manejar el error ya que cuando no hay alojamientos en la base de datos, el servidor devuelve un error 400, 401
+               if (error.response.status === 401) {
                     console.log("Error 401");
-               };
+                    return [];
+               } else {
+                    
+                    return [];
+               }
           }
-
-     //}
 
      return events;
 
-   } catch (error) {
-          console.log(error);
+   } catch (error: any) {
+           console.log(error);
           return [];
-   } 
+   }
 
    
 
 };
-
